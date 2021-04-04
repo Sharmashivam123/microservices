aab5aaa8c33be0ff1390e2dd3d25025386e9420c
shivamsharma.js@gmail.com <shivamsharma.js@gmail.com>
2021-04-04 16:25:05 +0530

    Updated repository to Paging And Sorting Crud Repository

--- src/main/java/com/explore/microservices/repo/TourRepo.java
+++ src/main/java/com/explore/microservices/repo/TourRepo.java
@@ -1,16 +1,14 @@
 package com.explore.microservices.repo;
 
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.explore.microservices.domain.Tour;
 
 @Repository
-public interface TourRepo extends CrudRepository<Tour, Integer>{
+public interface TourRepo extends PagingAndSortingRepository<Tour, Integer>{
 
-	List<Tour> findByTourPackageCode(String code);
+	Page<Tour> findByTourPackageCode(String code, org.springframework.data.domain.Pageable pageable);
 	
 }
