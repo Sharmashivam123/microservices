532135351d7a1e29aa357cfc1b6d5d3465754c72
shivamsharma.js@gmail.com <shivamsharma.js@gmail.com>
2021-04-04 16:59:14 +0530

    Upated repositry with @RestResources annotation to keet false  so we could restrict few of the operation like save and delete and also added hal-explorer dependency

--- src/main/java/com/explore/microservices/repo/TourRepo.java
+++ src/main/java/com/explore/microservices/repo/TourRepo.java
@@ -2,12 +2,55 @@ package com.explore.microservices.repo;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.explore.microservices.domain.Tour;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourRepo extends PagingAndSortingRepository<Tour, Integer>{
+
+	@Override
+	@RestResource(exported = false)
+	default <S extends Tour> S save(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default <S extends Tour> Iterable<S> saveAll(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void delete(Tour entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void deleteAll(Iterable<? extends Tour> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
 
 	Page<Tour> findByTourPackageCode(String code, org.springframework.data.domain.Pageable pageable);
 	
